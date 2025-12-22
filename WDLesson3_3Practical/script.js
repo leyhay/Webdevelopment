9e779217ec19d0a86d11d22192e342a347458bd6
leyhay <hayleyh9@nycstudents.net>
2025-12-22 14:16:55 +0000

    update3.3

--- WDLesson3_3Practical/script.js
+++ WDLesson3_3Practical/script.js
@@ -1,21 +1,21 @@
 /* Challenge 2: Create a function for each image that display a description of the picture into the div with an id of highlight*/
 function lee(){
-    alert("This is Lee know during the DO IT album.")
+    document.getElementById("highlight").innerHTML = "This is Lee know during the DO IT album.";
 }
 
 function know(){
-    alert("This is Lee know during the KARMA album.")
+    document.getElementById("highlight").innerHTML = "This is Lee know during the KARMA album.";
 }
 
 function minho(){
-    alert("This is a picture of Lee Know when he donated money to help others in need.")
+    document.getElementById("highlight").innerHTML = "This is a picture of Lee Know when he donated money to help others in need.";
 }
 
 function lino(){
-    alert("This is Lee know during promotion of DO IT on a music show.")
+    document.getElementById("highlight").innerHTML = "This is Lee know during promotion of DO IT on a music show.";
 }
 
 function skz(){
-    alert("This is when Lee know was selected as the Genius Idol")
+    document.getElementById("highlight").innerHTML = "This is when Lee know was selected as the Genius Idol.";
 }
 
