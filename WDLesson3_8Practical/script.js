40f0165cc13c9edc08f04f3595f8349c5fd7f5e8
leyhay <hayleyh9@nycstudents.net>
2026-01-29 14:33:37 +0000

    8

--- WDLesson3_8Practical/script.js
+++ WDLesson3_8Practical/script.js
@@ -5,17 +5,22 @@
 /* Challenge 4: Does the function name match the event handler referenced in the event listener? */
 function volume(){
 
-  /* Challenge 5: Are there any errors in retrieving and parsing the information from the text inputs? */
-  let r = parseInt(document.getElementBy("r").value);
-  let height = parseInt(document.getElementbyId("height").value);
+  /* Challenge 5: Are there any errors in retrieving and parsing the information from the text inputs?
+  forgot the Id in get.ElementById
+  b is lowercase */
+  let r = parseInt(document.getElementById("r").value);
+  let height = parseInt(document.getElementById("height").value);
 
-  /* Challenge 6: Does the variable output correctly create a reference to the output container? */
-  let output = document.getElementById("output").value;
+  /* Challenge 6: Does the variable output correctly create a reference to the output container? 
+  has dot value*/
+  let output = document.getElementById("output");
 
-  /* Challenge 7: Is the following calculation for volume of a cylinder correct? */
-  let v = math.PI * math.pow(r,2) / h;
+  /* Challenge 7: Is the following calculation for volume of a cylinder correct? 
+  did the math wrong*/
+  let v = math.PI * math.pow(r,2) * h;
 
-  /* Challenge 8: Are there any errors in displaying the output? */
-  output.innerHTML = `Volume of the cylinder is ${v} 
+  /* Challenge 8: Are there any errors in displaying the output? 
+  weird output*/
+  output.innerHTML = "Volume of the cylinder is " + v;
 
 }
