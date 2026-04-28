740e0015f92419d6fd5759707875fcc7d2cd5e66
leyhay <hayleyh9@nycstudents.net>
2026-04-28 13:33:54 +0000

    6.7update

--- WDLesson6_7Practical/script.js
+++ WDLesson6_7Practical/script.js
@@ -1,4 +1,18 @@
-function Area(){
-    let a = document.getElementById("area");
-    let output = document.getElementById("output");
+let data;
+async function init(){
+    let link = "311.json"
+    info = await fetch(link);
+    data = await info.json;
+    complaint = data[i];
+    
+    build = "";
+    for(let i=0;i<data.length;i+=1){
+        build += `<div class=card>
+                     <h2></h2>`
+    }
+
+    function Area(){
+        let a = document.getElementById("area");
+        let output = document.getElementById("output");
+}
 }
