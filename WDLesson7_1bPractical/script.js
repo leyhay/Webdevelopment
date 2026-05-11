aaeaed55234d5b7c8e771599c522fb21c440d18b
leyhay <hayleyh9@nycstudents.net>
2026-05-11 13:33:48 +0000

    save

--- WDLesson7_1bPractical/script.js
+++ WDLesson7_1bPractical/script.js
@@ -6,10 +6,13 @@ function init(){
   data = $.getJSON("data.js").responseJSON;
 
   output = get("output");
-
+  let build = "";
   //Challenge 2: Test the card( ) developed in Challenge 1 by displaying the first violation to the webpage.
-
-
+  for(let i = 0; i<data.length; i+=1){
+    let violation = data[i];
+    build += card(violation);
+  }
+output.innerHTML = build;
 
   //Challenge 4: Display all the Open Parking and Camera Violations to the web page using the cards( ) from Challenge 3.
   
